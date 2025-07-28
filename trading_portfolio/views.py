2e9dbffd709df138f14071a1ab11ab09ed4b4332
Clebert <clebertcivil01@gmail.co>
2025-07-28 04:28:29 -0400

    create login functionality and added the dashboard template page

--- trading_portfolio/views.py
+++ trading_portfolio/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from trading_portfolio.forms import SignUpForm
 
@@ -8,13 +9,48 @@ from trading_portfolio.forms import SignUpForm
 def home(request):
     return render(request, 'home.html', {})
 
+
 def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            # add Authenticate and login functionality in register
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('dashboard')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form': form})
+        return render(request, 'accounts/register.html', {'form': form})
 
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('dashboard')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out!")
+    return redirect('home')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html', context={'user': request.user})
