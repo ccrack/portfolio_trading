e9e9d82829bc01d803bf61ad85428a38edce5c7f
ccrack <clebertcivil01@gmail.com>
2025-08-28 20:24:17 -0400

    connect my app to aivencloud web server to store my data

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,7 +18,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,21 +70,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db',
-        'USER': 'root',
-        'PASSWORD': 'Password1234',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Password1234',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
+# Add Aiven SSL options (mysqlclient expects this format)
+DATABASES["default"]["OPTIONS"] = {
+    "ssl": {
+        "ca": "/etc/ssl/certs/ca-certificates.crt"  # path to CA bundle
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -106,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -117,7 +119,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
